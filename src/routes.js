bd1fd488c4904ce5567e7d464338944586c3606f
luismatos19 <luismatosrobot@gmail.com>
2021-06-03 16:35:55 -0300

    pagina login

--- src/routes.js
+++ src/routes.js
@@ -2,8 +2,16 @@ const express = require("express");
 const routes = express.Router();
 const HomeController = require("./controllers/HomeController");
 const ContactController = require("./controllers/ContactController");
+const LoginController = require("./controllers/LoginController");
 
+//Home Routes
 routes.get("/", HomeController.index);
+
+//Login Routes
+routes.get("/login", LoginController.index);
+routes.post("/login/register", LoginController.register);
+
+//Contact Routes
 routes.get("/contact", ContactController.create);
 
 module.exports = routes;
